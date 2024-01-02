f6782fdc6015f42473f78031c1c63a71574ba0f0
Jeff77789 <jeff.ding777@gmail.com>
2024-01-01 23:04:25 -0500

    Version 1.1.1 fixed wifi naming multiple devices

--- SwitchControlBoard_code/src/main.cpp
+++ SwitchControlBoard_code/src/main.cpp
@@ -1,6 +1,6 @@
 // DingKey Designs Control Board
 // 1/1/2023
-#define SW_VERSION "v1.1.1beta"
+#define SW_VERSION "v1.1.1"
 
 #include <Arduino.h>
 #include <SPI.h>
@@ -358,24 +358,12 @@ void setup() {
     {
         Serial.print(n);
         Serial.println(" Networks found");
-        // for (int i = 0; i < n; ++i){
-        // Serial.println(WiFi.SSID(i)); // Print SSID and RSSI for each network found, not in alphabetical order
-        // std::string wifi_name = WiFi.SSID(i).c_str();
-        // std::regex wifi_expr("DingKeyWifi.?[0-9]*"); // Matches DingKeyWifi format
-        // std::smatch wifi_match;
-        // if (std::regex_match(wifi_name, wifi_match, wifi_expr)){
-        // // if (WiFi.SSID(i) == ssidRand) {//enter the ssid that matches assigned name
-        //         Serial.println("Existing DingKey network found");
-        //         id_suffix++; // increment to next wifi suffix
-        //         snprintf(ssidRand,25,"%s-%04d",APSSID,id_suffix); //update with newest increment
-        //         if (WiFi.SSID(i) != ssidRand) {//enter the ssid that matches assigned name
-        //             break; // exit for loop if incremented wifi name is available
-        //         }
-        //     }
-        // }
+        // Print list of wifi networks
         for (int i = 0; i < n; ++i){ // iterate through list of WiFi names
             Serial.println(WiFi.SSID(i)); // Print SSID and RSSI for each network found, not in alphabetical order
         }
+
+        // Find availalbe wifi name
         while (true){
             int matches = 0; // number of WiFi name matches
             for (int i = 0; i < n; ++i){ // iterate through list of WiFi names
